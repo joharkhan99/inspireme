ca61c80def58745fe82322090b4f4bbd18d6d0b3
single-byte <70197335+single-byte@users.noreply.github.com>
2023-07-06 13:00:27 +0500

    added new abb and apk

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import React, { useEffect, useState } from "react";
 import StackNavigator from "./components/StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
+import { ActivityIndicator, View } from "react-native";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay to showcase the loader
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Replace this with your actual loading logic
+  }, []);
+
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#E11A38" />
+      ) : (
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      )}
+    </View>
   );
 }
